93c845b8c4913c8ef1af6c33a2f90cb6966fbcce
Ryan Mitchell <ryanand26@gmail.com>
2013-06-26 15:01:25 +0100

    Added further test cases for event handling

--- tests/models/looseValidation.test.js
+++ tests/models/looseValidation.test.js
@@ -1,7 +1,7 @@
 suite('Validator Contructor', function() {
 
     setup(function() {
-        this.formSelect = '.looseValidation';
+        this.formSelect = '.ruleTests';
         /*this.user = new app.models.User({
             first_name: "Jimmy",
             last_name: "Wilson"
@@ -34,25 +34,23 @@ suite('Validator Contructor', function() {
 
 });
 
+/**
+* Test the internal validation rules directly
+*/
 suite('Validator Test Functions', function() {
 
     setup(function() {
-        this.formSelect = '.looseValidation';
+        this.formSelect = '.ruleTests';
         this.form = $(this.formSelect).looseValidation();
-        this.textfield = $('#required');
-        this.select = $('#required-select');
-        this.checkbox = $('#required-checkbox');
-        this.textarea = $('#required-textarea');
+        this.textfield = $('#required-a');
+        this.select = $('#required-select-a');
+        this.checkbox = $('#required-checkbox-a');
+        this.textarea = $('#required-textarea-a');
 
         this.validator = this.form.data('validator');
     });
 
     teardown(function() {
-        this.textfield.val('');
-        this.select[0].selectedIndex = 0;
-        this.checkbox[0].checked = false;
-        this.textarea[0].value = '';
-
         this.validator = null;
         this.form = null;
     });
@@ -100,5 +98,88 @@ suite('Validator Test Functions', function() {
         expect(this.validator.useRule('required', this.textarea)).to.be.false;
     });
 
+
+});
+
+/**
+* Test the rule effects through the UI
+*/
+suite('Validator Binding', function() {
+
+    setup(function() {
+        this.formSelect = '.looseValidation';
+        this.form = $(this.formSelect).looseValidation();
+        this.textfield = $('#required');
+        this.select = $('#required-select');
+
+        this.validator = this.form.data('validator');
+    });
+
+    teardown(function() {
+        this.textfield.val('');
+        this.select[0].selectedIndex = 0;
+        this.checkbox[0].checked = false;
+        this.textarea[0].value = '';
+
+        this.validator = null;
+        this.form = null;
+    });
+
+    test("validateField : Textfield", function() {
+        //empty textfield fails
+        this.textfield.val('');
+        //call manually
+        this.validator.validateField(this.textfield);
+
+        expect(this.textfield.attr('aria-invalid')).to.equal('true');
+
+
+        //filled textfield passes
+        this.textfield.val('this is a value');
+        //call manually
+        this.validator.validateField(this.textfield);
+
+        expect(this.textfield.attr('aria-invalid')).to.equal('false');
+    });
+
+    test("validateField : Textfield modified (keyup) to be emptys fails on blur", function() {
+        var event = jQuery.Event("keyup");
+        event.keyCode = 33; //keycode 33 === !
+
+        //empty textfield fails
+        this.textfield.val();
+        //fire a mock keypress to trigger the event chain but not change the field value
+        this.textfield.trigger(event).trigger('blur');
+
+        //expect(this.validator.validateField(this.textfield)).to.be.false;
+        expect(this.textfield.attr('aria-invalid')).to.equal('true');
+    });
+
+    test("validateField : Textfield modified (keyup) with content succeeds on blur", function() {
+        var event = jQuery.Event("keyup");
+        event.keyCode = 33; //keycode 33 === !
+
+        //filled textfield fails
+        this.textfield.val('i am a value');
+        //fire a mock keypress to trigger the event chain but not change the field value
+        this.textfield.trigger(event).trigger('blur');
+
+        expect(this.textfield.attr('aria-invalid')).to.equal('false');
+    });
+
+    test("validateField : Selectbox validated on blur or change", function() {
+        //fire a mock keypress to trigger the event chain but not change the field value
+        this.select.trigger('blur');
+
+        expect(this.select.attr('aria-invalid')).to.equal('true');
+
+        //change to a valid value for the next test
+        this.select[0].selectedIndex = 1;
+
+        //fire a the change event manually as changing it in JS doesnt fire it
+        this.select.trigger('change');
+
+        expect(this.select.attr('aria-invalid')).to.equal('false');
+    });
 
 });
